882fc2eb0c2f87e38c54c9d43a9483a7d1565a55
K3rn4lP4nic <149522852+K3rn4lP4nic@users.noreply.github.com>
2025-05-04 09:37:59 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hey there! I'm Sai.`}
+  <br />
+  <br />
+  {`With 4 years of experience as a penetration tester, my journey in tech has been anything but ordinary. I've always been drawn to the intricate layers of computers and cybersecurity — not just as a profession, but as a deep-rooted passion.`}
+  <br />
+  <br />
+  {`Growing up with early access to a computer felt like unlocking a new world. From the pixelated charm of '90s games to the structured complexity of modern-day security systems, every chapter of this journey has shaped me. It's a beautiful transition — one that constantly reminds me to stay grateful and keep embracing the path that led me here.`}
       </p>
       <div className="my-8">
         <BlogPosts />
